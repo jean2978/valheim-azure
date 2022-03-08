4e7f604f674c656e881967704c1c514726ee1cdd
Jesper Andersson <37796779+jean2978@users.noreply.github.com>
2022-03-08 16:11:10 +0100

    feat: added container group configuration

--- main.tf
+++ main.tf
@@ -26,3 +26,33 @@ resource "azurerm_storage_account" "storage" {
   name                     = "${var.project_name}-storage"
   resource_group_name      = var.resource_group_name
 }
+
+resource "azurerm_container_group" "container" {
+  location            = var.locaion
+  name                = "valheimserver"
+  os_type             = "Linux"
+  resource_group_name = var.resource_group_name
+  ip_address_type = "public"
+  dns_name_label = "<something>"
+  exposed_port {
+    port = 2456
+    protocol = "udp"
+  }
+  exposed_port {
+    port = 2457
+    protocol = "udp"
+  }
+  container {
+    cpu    = 2
+    image  = "lloesche/valheim-server"
+    memory = 4
+    name   = "valheimserver"
+    volume {
+      mount_path = "/config"
+      name       = "valheim-server-data"
+      //storage configuration
+      storage_account_key = ""
+      storage_account_name = ""
+    }
+  }
+}
